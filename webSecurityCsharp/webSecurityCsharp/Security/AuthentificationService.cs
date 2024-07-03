65689b258188683c748bebcd854de7b1f45b4ff6
Thiago Andrade <thiagoandrade160302@gmail.com>
2024-07-03 18:06:11 -0300

    Create a Jwt token

--- webSecurityCsharp/webSecurityCsharp/Security/AuthentificationService.cs
+++ webSecurityCsharp/webSecurityCsharp/Security/AuthentificationService.cs
@@ -1,31 +1,28 @@
 ﻿using System.IdentityModel.Tokens.Jwt;
 using webSecurityCsharp.Entitys;
-namespace webSecurityCsharp.Security
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+namespace webSecurityCsharp.Security;
+
+public class AuthentificationService()
 {
-    public class AuthentificationService()
-    {
-    }
-
-    private string GenerateJwtToken(User user)
-    {
-        var tokenHandler = new JwtSecurityTokenHandler();
-        var key = Enconding.UTF8.Get
-
-
-
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
+
+public string GenerateJwtToken(User user)
+{
+    var tokenHandler = new JwtSecurityTokenHandler();
+    var key = Encoding.UTF8.GetBytes(Globals.JWT_KEY);
+    var tokenDescriptor = new SecurityTokenDescriptor
+    {
+        Subject = new ClaimsIdentity([
+            new Claim(ClaimTypes.Name, user.Name),
+            new Claim(ClaimTypes.Email, user.Email),
+            ]),
+        Expires = DateTime.UtcNow.AddDays(7),
+        SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+    };
+    var token = tokenHandler.CreateToken(tokenDescriptor);
+    return tokenHandler.WriteToken(token);
+}
+
---
+++ webSecurityCsharp/webSecurityCsharp/Security/Globals.cs
@@ -0,0 +1,7 @@
+﻿namespace webSecurityCsharp.Security
+{
+    public static class Globals
+    {
+        public const string JWT_KEY = "asdfçlmnçlhpç2hpomlnzx;.cv~lj6543130164ASDF3XC16AS54FDG654641321ASDF654321XCV695849847RASGH3XS51361ASDVa~lçskujrpoí3urojça.ld;.,mnxçljhnasdlçhfj";
+    }
+}
