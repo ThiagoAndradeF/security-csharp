5f290489459561eb6482ccc547d985a525ecb6ba
Thiago Andrade Ferreira <thiagoandrade160302@gmail.com>
2024-07-03 11:13:22 -0300

    Init Repository

---
+++ webSecurityCsharp/webSecurityCsharp/Entitys/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace webSecurityCsharp.Entitys
+{
+    public class User
+    {
+        public required string Name { get; set; }
+
+        public required string Email { get; set; }
+
+        public byte[] PasswordHash { get; set; }
+
+        public byte[] PasswordSalt { get; set; }
+
+        public bool ShouldUpdatePassword { get; set; }
+
+        public bool IsActive { get; set; }
+
+    }
+}
---
+++ webSecurityCsharp/webSecurityCsharp/Security/AuthentificationService.cs
@@ -0,0 +1,26 @@
+﻿using webSecurityCsharp.Entitys;
+namespace webSecurityCsharp.Security
+{
+    public class AuthentificationService()
+    {
+    }
+
+    private string GenerateJwtToken(User user)
+    {
+        var tokenHandler = new 
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
