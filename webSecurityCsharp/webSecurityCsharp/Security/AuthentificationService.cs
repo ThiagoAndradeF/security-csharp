8a4e816112bd51ee69c771727c289a75e4376fdd
Thiago Andrade <thiagoandrade160302@gmail.com>
2024-07-03 18:10:41 -0300

    Create a password hash
    sault and hash password implemented

--- webSecurityCsharp/webSecurityCsharp/Security/AuthentificationService.cs
+++ webSecurityCsharp/webSecurityCsharp/Security/AuthentificationService.cs
@@ -7,22 +7,28 @@ namespace webSecurityCsharp.Security;
 
 public class AuthentificationService()
 {
-}
-
-public string GenerateJwtToken(User user)
-{
-    var tokenHandler = new JwtSecurityTokenHandler();
-    var key = Encoding.UTF8.GetBytes(Globals.JWT_KEY);
-    var tokenDescriptor = new SecurityTokenDescriptor
+    public string GenerateJwtToken(User user)
     {
-        Subject = new ClaimsIdentity([
-            new Claim(ClaimTypes.Name, user.Name),
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.UTF8.GetBytes(Globals.JWT_KEY);
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity([
+                new Claim(ClaimTypes.Name, user.Name),
             new Claim(ClaimTypes.Email, user.Email),
             ]),
-        Expires = DateTime.UtcNow.AddDays(7),
-        SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
-    };
-    var token = tokenHandler.CreateToken(tokenDescriptor);
-    return tokenHandler.WriteToken(token);
+            Expires = DateTime.UtcNow.AddDays(7),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+        };
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
 }
+
+
+
+
+
+
+
 
